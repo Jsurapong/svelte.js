686c57e3ce99c5c0b5ce617eaace5dc3086bbab2
Surapong Kawkangploo <surapongkawkangploo@Surapongs-MacBook-Pro.local>
2022-05-26 15:28:20 +0700

    config .vscode

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+  },
+  "plugins": [
+    "prettier"
+  ],
+};
