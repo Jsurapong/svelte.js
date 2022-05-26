6b420c241b3bbadfb26afa5d0ab05216daaf9ad3
Surapong Kawkangploo <surapongkawkangploo@Surapongs-MacBook-Pro.local>
2022-05-26 15:44:11 +0700

    with out $:

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,27 @@
 <script>
-	let name = 'Max';
-	let age = 30;
+  let name = "Surapong";
+  let age = 30;
 
-	function incrementAge() {
-		age += 1;
-	}
+  function incrementAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = "Surapong Kawkangploo";
+  }
+
+  $: uppercaseName = name.toUpperCase();
+  $: console.log("$:", name);
+
+  console.log("with out $:", name);
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<button on:click={changeName}>ChangeName</button>
 
-<h1>Hello {name}, my age is {age}!</h1>
-<button on:click="{incrementAge}">Change Age</button>
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
