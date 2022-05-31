7c660504e121ad98163943288832aad6b4aad6f1
Surapong Kawkangploo <surapongkawkangploo@Surapongs-MacBook-Pro.local>
2022-05-31 15:12:55 +0700

    60 using spread props & default props

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
-	export let name;
+  import Product from "./Product.svelte";
+
+  let products = [{ id: "p1", title: "A book", price: 99.99 }];
+
+  function addToCart(event) {
+    console.log(event);
+  }
+
+  function deleteProduct(event) {
+    console.log(event.detail);
+  }
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+{#each products as product}
+  <Product
+    title={product.title}
+    price={product.price}
+    on:add-to-cart={addToCart}
+    on:delete={deleteProduct}
+  />
+{/each}
