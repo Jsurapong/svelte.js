9deb11873aaba38a177b5050bb8fc59f38b73e38
Surapong Kawkangploo <surapongkawkangploo@Surapongs-MacBook-Pro.local>
2022-05-31 16:06:04 +0700

    76 using tick

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { tick, afterUpdate } from "svelte";
   import Product from "./Product.svelte";
   import Modal from "./Modal.svelte";
 
@@ -7,12 +8,36 @@
   let showModal = false;
   let closeable = false;
 
+  let text = "This is some dummy text!";
+
   function addToCart(event) {
     console.log(event);
   }
 
   function deleteProduct(event) {
     console.log(event.detail);
+  }
+
+  function transform(event) {
+    console.log(event);
+
+    if (event.which !== 9) return;
+
+    event.preventDefault();
+
+    const selectionStart = event.target.selectionStart;
+    const selectionEnd = event.target.selectionEnd;
+
+    const value = event.target.value;
+    text =
+      value.slice(0, selectionStart) +
+      value.slice(selectionStart, selectionEnd).toUpperCase() +
+      value.slice(selectionEnd);
+
+    tick().then(() => {
+      event.target.selectionStart = selectionStart;
+      event.target.selectionEnd = selectionEnd;
+    });
   }
 </script>
 
@@ -48,3 +73,5 @@
     >
   </Modal>
 {/if}
+
+<textarea rows="5" value={text} on:keydown={transform} />
