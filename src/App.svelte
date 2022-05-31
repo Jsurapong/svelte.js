5284c54deccb43ba2b7899b660a89e90b9d71c3d
Surapong Kawkangploo <surapongkawkangploo@Surapongs-MacBook-Pro.local>
2022-05-31 12:16:42 +0700

    51 adding a custom button component'

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import Header from "./UI/Header.svelte";
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
   import TextInput from "./UI/TextInput.svelte";
+  import Button from "./UI/Button.svelte";
 
   let meetups = [
     {
@@ -96,7 +97,7 @@
         value={description}
         on:input={(event) => (description = event.target.value)}
       />
-      <button type="submit">Save</button>
+      <Button type="submit" caption="Save" />
     </form>
     <MeetupGrid {meetups} />
   </form>
