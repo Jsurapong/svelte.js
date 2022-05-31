172622181498d62d7abe642fc0e685d2e58cb3f9
Surapong Kawkangploo <surapongkawkangploo@Surapongs-MacBook-Pro.local>
2022-05-31 11:50:38 +0700

    74 passing data into components

--- src/App.svelte
+++ src/App.svelte
@@ -31,7 +31,14 @@
 
 <section id="meetups">
   {#each meetups as meetup}
-    <MeetupItem />
+    <MeetupItem
+      title={meetup.title}
+      subtitle={meetup.subtitle}
+      imageUrl={meetup.imageUrl}
+      description={meetup.description}
+      address={meetup.address}
+      email={meetup.contactEmail}
+    />
   {/each}
 </section>
 
